e6baa86b16d7e81829bc46a180060e1a4fea530f
Reynolito Reso-or <reynolitoresoor123@gmail.com>
2026-05-18 19:48:25 +0800

    Update nav

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,7 +1,7 @@
 export function Nav() {
     return (
         <nav className="w-full flex justify-center h-16">
-            <div className="w-full max-w-5xl flex justify-center items-center border-b border-b-foreground/10 p-3 px-5 text-sm">
+            <div className="w-full flex justify-center items-center border-b border-b-foreground/10 p-3 px-5 text-sm">
             <div className="flex justify-center gap-2 md:gap-10 lg:gap-x-12">
                 <a href="#about" className="text-sm/6 font-semibold text-gray-900">About</a>
                 <a href="#work-experience" className="text-sm/6 font-semibold text-gray-900">Work Experience</a>
