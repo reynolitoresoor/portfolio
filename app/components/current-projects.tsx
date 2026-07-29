d7007ef3a924e9839c9760f6f446af2aa5e3895f
Reynolito Reso-or <reynolitoresoor123@gmail.com>
2026-07-29 20:27:34 +0800

    Update current project links to new tab

--- app/components/current-projects.tsx
+++ app/components/current-projects.tsx
@@ -6,7 +6,7 @@ export function CurrentProjects() {
         <div className="flex flex-col" key={index}>
             <h3 className="text-lg font-semibold tracking-tight text-balance text-gray-900">{project.name}</h3>
             <p className="text-gray-500">{project.description}</p>
-            <a href={project.sourceCode}><img src={project.img} width="200" title={project.name} alt={project.name} className="mt-2"/></a>
+            <a href={project.sourceCode} target="_blank"><img src={project.img} width="200" title={project.name} alt={project.name} className="mt-2"/></a>
         </div>
         ))}
        </>
