a41c7a31bf5bc0631230c38794bd1dd7305813e5
Reynolito Reso-or <reynolitoresoor123@gmail.com>
2026-08-22 02:04:14 +0800

    Update profile link

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -8,7 +8,7 @@ export function Hero() {
       <div className="w-full flex flex-col gap-4 items-center">
         <div className="flex flex-col md:fixed lg:fixed top-20 md:mt-5 lg:mt-5 md:pt-5 md:pt-5 items-center">
             <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+            href="/"
             target="_blank"
             rel="noreferrer"
           >
